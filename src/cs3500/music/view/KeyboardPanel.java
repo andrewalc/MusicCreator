04d3bcb54cb7aee35ef622ede41251e11f7fea6b
andrewalc <andrewalc98@gmail.com>
2017-06-15 00:44:07 -0400

    Fixed white keys not updating switchCount at all.

--- src/cs3500/music/view/KeyboardPanel.java
+++ src/cs3500/music/view/KeyboardPanel.java
@@ -16,6 +16,7 @@ public class KeyboardPanel extends JPanel {
 
   // @TODO dont need to us public static everywhere
   public static final int NUM_WHITEKEYS = 70;
+  public static final int NUM_BLACKKEYS = 50;
   public static final int BLKEY_WIDTH = 10;
   public static final int BLKEY_HEIGHT = 150;
   public static final int KEY_WIDTH = 20;
@@ -54,9 +55,9 @@ public class KeyboardPanel extends JPanel {
     int switchCounter = 1;
 
     for (int i = 0; i < NUM_WHITEKEYS; i++) {
-
       for (Integer pitch : currentPitches) {
         if (keyNumber == pitch) {
+
           g.setColor(Color.orange);
           g.fillRect((i * KEY_WIDTH) + EditorPanel.BORDER_SHIFT,
                   0, KEY_WIDTH, KEY_HEIGHT);
@@ -67,13 +68,16 @@ public class KeyboardPanel extends JPanel {
               0, KEY_WIDTH, KEY_HEIGHT);
 
 
-      if (switchCounter == 3 || switchCounter == 7) {
+      if (switchCounter == 3 || switchCounter == 7) { // may be 3/6 or 2/7
         keyNumber += 1;
         if (switchCounter == 7) {
           switchCounter = 1;
+        } else {
+          switchCounter += 1;
         }
       } else {
         keyNumber += 2;
+        switchCounter += 1;
       }
     }
 
@@ -82,10 +86,8 @@ public class KeyboardPanel extends JPanel {
 
     // generates the double set of black keys
     int numBlackKey = 1;
-    for (int i = 0; i < NUM_WHITEKEYS / 2; i++) {
-      if (numBlackKey >= NUM_WHITEKEYS) {
-        break;
-      } else {
+    for (int i = 1; i <= NUM_WHITEKEYS; i++) {
+      for (int j = 0; j < NUM_BLACKKEYS; j++) {
         g.fillRect(numBlackKey * KEY_WIDTH - (BLKEY_WIDTH / 2) + EditorPanel.BORDER_SHIFT,
                 0,
                 BLKEY_WIDTH,
@@ -96,29 +98,8 @@ public class KeyboardPanel extends JPanel {
                 BLKEY_HEIGHT);
         numBlackKey += 6;
       }
-
     }
 
-    // generates the triple set of black keys
-    numBlackKey = 4;
-    for (int i = 0; i < NUM_WHITEKEYS / 3; i++) {
-      if (numBlackKey >= NUM_WHITEKEYS) {
-        break;
-      } else {
-        g.fillRect(numBlackKey * KEY_WIDTH - (BLKEY_WIDTH / 2) + EditorPanel.BORDER_SHIFT, 0,
-                BLKEY_WIDTH,
-                BLKEY_HEIGHT);
-        numBlackKey++;
-        g.fillRect(numBlackKey * KEY_WIDTH - (BLKEY_WIDTH / 2) + EditorPanel.BORDER_SHIFT, 0,
-                BLKEY_WIDTH,
-                BLKEY_HEIGHT);
-        numBlackKey++;
-        g.fillRect(numBlackKey * KEY_WIDTH - (BLKEY_WIDTH / 2) + EditorPanel.BORDER_SHIFT, 0,
-                BLKEY_WIDTH,
-                BLKEY_HEIGHT);
-        numBlackKey += 5;
-      }
-    }
   }
 
 }
